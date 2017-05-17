f8a94e7ee66fc0a9d6a328855650564bfe47d293
Yashicataneja <yashica.taneja@cmss.in>
2017-05-17 15:24:19 +0530

    Calculating price of coffee with constant value

---
+++ app/src/main/java/com/example/cmss/userinput/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.cmss.userinput;
+
+import android.icu.text.NumberFormat;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void submitOrder(View view) {
+        int Quantity = 2;
+        display(Quantity);
+        displayPrice(Quantity * 5);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+    private void display(int number) {
+        TextView quantityTextView =(TextView)findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(" " +number);
+    }
+}
